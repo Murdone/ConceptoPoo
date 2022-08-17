f0fa0aa25f8b16de0a1b29ef63141ac603921dd5
Murdo <vasquez.percibal@gmail.com>
2022-08-16 21:14:39 -0400

    Finish date Class

---
+++ ConceptoPOO/ConceptoPOO/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("_____________");
+Console.WriteLine("Hola Mundo");
+Console.WriteLine("_____________");
+
+try
+{
+	Console.WriteLine(new Date(2024, 2, 29));
+	Console.WriteLine(new Date(1957, 9, 23));
+	Console.WriteLine(new Date(1985, 11, 5));
+}
+catch (Exception error)
+{
+
+	Console.WriteLine(error.Message);
+}
+Console.ReadKey();
